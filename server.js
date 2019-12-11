b7836eea741bbfd96c5f6dcc6b795234a7670c4b
Daykel Muro <daykelms@gmailcom>
2019-12-11 16:08:30 -0500

    send ui created

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-
-app.use(express.static(__dirname))
-var server = app.listen(3000, ()=>{console.log('server is running in port', server.address().port)})
+app.use(express.static(__dirname));
+var server = app.listen(3000, () => {
+  console.log("server is running in port", server.address().port);
+});
