9b4c8a3a549eea50e0ddc9c316e89ec3fe3e2a92
Daykel Muro <daykelms@gmailcom>
2019-12-11 16:36:07 -0500

    back end connections

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require("express");
+var bodyParser = require('body-parser')
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json())
+
+var messages =[
+    { name: 'Daykel', message: 'Hi'},
+    { name: 'Pedro', message: 'Hola'}
+]
+
+app.get('/messages', (req, res)=>{
+    res.send(messages)
+ })
+
+app.post('/messages', (req, res)=>{
+    console.log(req.body)
+    res.sendStatus(200)
+ })
+
 var server = app.listen(3000, () => {
   console.log("server is running in port", server.address().port);
 });
