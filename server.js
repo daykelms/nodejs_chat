da4fe59183f4b75671688307d3800831f9859aaa
Daykel Muro <daykelms@gmailcom>
2019-12-12 15:34:33 -0500

    latest changes up to 7

--- server.js
+++ server.js
@@ -1,37 +1,57 @@
-
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
-var io = require('socket.io-client')(http)
+var io = require('socket.io')(http)
 var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-var dbUrl = 'mongodb+srv://user:user@nodejschat-47kgn.mongodb.net/test?retryWrites=true&w=majority'
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/messages', (req, res) =>{
-    res.send(messages)
+var dbUrl = 'mongodb+srv://user:user@nodejschat-47kgn.mongodb.net/test?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
 })
 
-app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    })
+})
+
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if (err)
+            sendStatus(500)
+
+        Message.findOne({message: 'badword'}, (err, censored) => {
+            if(censored) {
+                console.log('censored words found', censored)
+                Message.remove({_id: censored.id}, (err) =>{
+                    console.log('removed censored message')
+                })
+            }
+        })
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
-mongoose.connect(dbUrl, {useUnifiedTopology: true, useNewUrlParser: true } ,(err) =>{
-  console.log('a user connected', err)
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+    console.log('mongo db connection', err)
 })
+
 var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
