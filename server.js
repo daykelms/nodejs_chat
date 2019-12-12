73fa18c471075bbd7ccc7cf5503fa3c9d12ca81b
Daykel Muro <daykelms@gmailcom>
2019-12-12 10:53:43 -0500

    mongo db added

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
-var express = require("express");
-var bodyParser = require('body-parser')
-var app = express();
 
-app.use(express.static(__dirname));
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io-client')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
-var messages =[
-    { name: 'Daykel', message: 'Hi'},
-    { name: 'Pedro', message: 'Hola'}
+var dbUrl = 'mongodb+srv://user:user@nodejschat-47kgn.mongodb.net/test?retryWrites=true&w=majority'
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
 ]
 
-app.get('/messages', (req, res)=>{
+app.get('/messages', (req, res) =>{
     res.send(messages)
- })
+})
 
-app.post('/messages', (req, res)=>{
+app.post('/messages', (req, res) =>{
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
- })
+})
 
-var server = app.listen(3000, () => {
-  console.log("server is running in port", server.address().port);
-});
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, {useUnifiedTopology: true, useNewUrlParser: true } ,(err) =>{
+  console.log('a user connected', err)
+})
+var server = http.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
+})
